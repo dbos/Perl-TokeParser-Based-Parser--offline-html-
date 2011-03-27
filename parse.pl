4ea733928f270b4d0f62f030989498005e71244d
Danny O'Sullivan <danny.osullivan@gmail.com>
2011-03-26 21:50:24 -0400

    Abstracted Script, Provided Sample File

---
+++ parse.pl
@@ -0,0 +1,53 @@
+#!/bin/perl -w
+# Author: Danny O'Sullivan
+# Dependencies: HTML::TokeParser, String:Strip (both on CPAN)
+# Usage: parse.pl [filename]
+
+# This parser works only on offline files, so no there is no LWP Dependency. 
+# See index.html provided for template input file
+
+use strict;
+use warnings;
+use HTML::TokeParser;
+use String::Strip;
+open(my $fh, "<:utf8",shift || 'example.html' ) || die "Cannot open file:$!";
+my $p=HTML::TokeParser->new($fh);
+my %myHash;
+my $firstField;
+my $hashKey;
+
+# The idea is that I find much of the field data before I am ready to create my hash entry.
+# Fortunately, the data is sequential, so lexical variables are used for storage until 
+# everything is ready. 
+
+while (my $token = $p->get_tag){
+	if ($token->[0] eq "strong" ){
+		$token=$p->get_token;
+		my $textBlock=$token->[1];
+		StripSpace($textBlock);
+		do {$firstField= $textBlock; $token = $p->get_tag('p');} if $textBlock=~ /^[\s\d]*$/s;
+		$token=$p->get_token;
+		$textBlock=$token->[1];
+		if ($textBlock=~ /^(.*)\((\d*)\)$/s){
+			$textBlock=$1;
+			$hashKey=$2;
+			StripSpace($hashKey);
+			$myHash{$hashKey}{firstField}=$firstField;
+			$myHash{$hashKey}{secondField}="";
+			$myHash{$hashKey}{text}="";
+		}
+		if ($textBlock=~ /^(.*)Value Rating:(.*)$/s){
+			$textBlock=$1;
+			my $secondField=$2;
+			StripSpace($secondField);
+			$myHash{$hashKey}{secondField}=$secondField;
+			$myHash{$hashKey}{text}=$textBlock;
+		}
+		$p->get_tag('strong');
+		$token=$p->get_token;
+		my $thirdField= $token->[1];
+		StripSpace($thirdField);
+		$myHash{$hashKey}{thirdField}=$thirdField;
+	}
+}
+print "Hash Key:\t$_\nFirst Field:\t$myHash{$_}{firstField}\nSecond Field:\t$myHash{$_}{secondField}\nThird Field:\t$myHash{$_}{thirdField}\nRemaining Text:\t$myHash{$_}{text}\n" for keys(%myHash);
